67354b1323cd88f5cdc1ba74ae816d33d253c7a0
Zwe Lwin Oo <167851320+zwe-lwin-00@users.noreply.github.com>
2024-11-07 16:27:32 +0630

    Update TranacationRepository.java

--- src/main/java/com/thirdeye/code/repository/TranacationRepository.java
+++ src/main/java/com/thirdeye/code/repository/TranacationRepository.java
@@ -1,14 +1,26 @@
 package com.thirdeye.code.repository;
 
+import java.util.List;
+
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 import org.springframework.stereotype.Repository;
 
 import com.thirdeye.code.entity.Tranacation;
 
-import jakarta.transaction.Transaction;
-
 @Repository
 public interface TranacationRepository extends JpaRepository<Tranacation, Long> {
 
-    Transaction findtransbynumber(int luckyNumber);
+
+    // @Query("SELECT t FROM Tranacation t WHERE t.buynumber = :luckyNumber")
+    // List<Tranacation> findtransbynumber(@Param("luckyNumber") int luckyNumber);
+
+
+    @Query("SELECT t FROM Tranacation t WHERE t.buynumber = :luckyNumber")
+    List<Tranacation> findtransbynumber(@Param("luckyNumber") int luckyNumber);
+
+
+
+    
 }

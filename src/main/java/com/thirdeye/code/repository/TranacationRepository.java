afb892e92a07c2e2dbb55e59c31c2db0a2f448ae
Zwe Lwin Oo <167851320+zwe-lwin-00@users.noreply.github.com>
2024-11-11 16:28:51 +0630

    Update TranacationRepository.java

--- src/main/java/com/thirdeye/code/repository/TranacationRepository.java
+++ src/main/java/com/thirdeye/code/repository/TranacationRepository.java
@@ -7,6 +7,7 @@ import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.query.Param;
 import org.springframework.stereotype.Repository;
 
+import com.thirdeye.code.dto.transcationdto;
 import com.thirdeye.code.entity.Tranacation;
 
 @Repository
@@ -19,6 +20,25 @@ public interface TranacationRepository extends JpaRepository<Tranacation, Long> 
 
     @Query("SELECT t FROM Tranacation t WHERE t.buynumber = :luckyNumber")
     List<Tranacation> findtransbynumber(@Param("luckyNumber") int luckyNumber);
+
+    
+
+    @Query("SELECT t.buynumber, t.customer.customerid, SUM(t.amount) " +
+       "FROM Tranacation t WHERE t.buynumber = :luckyNumber " +
+       "GROUP BY t.buynumber, t.customer.customerid")
+    List<Object[]> findtransactionsgroupbycus(@Param("luckyNumber") int luckyNumber);
+
+    
+
+    @Query("SELECT t.buynumber, t.customer.customerid, SUM(t.amount) " +
+       "FROM Tranacation t WHERE t.buynumber = :luckyNumber " +
+       "GROUP BY t.buynumber, t.customer.customerid")
+    List<transcationdto> findtransactionsgroupbycus5(@Param("luckyNumber") int luckyNumber);
+
+
+
+
+
 
 
 

bbf8550403dc7ab18476bab017cb54e333217497
Zwe Lwin Oo <167851320+zwe-lwin-00@users.noreply.github.com>
2024-11-12 11:28:09 +0630

    Update MainController.java

--- src/main/java/com/thirdeye/code/controller/MainController.java
+++ src/main/java/com/thirdeye/code/controller/MainController.java
@@ -1,11 +1,14 @@
 package com.thirdeye.code.controller;
 
+import java.util.List;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 
+import com.thirdeye.code.entity.Break;
 import com.thirdeye.code.service.BreakService;
 
 @Controller
@@ -18,6 +21,13 @@ public class MainController {
     // List all breaks
     @GetMapping
     public String listBreaks(Model model) {
+        //for disable break add
+        List<Break> breaks = breakService.findAll();
+        if (breaks != null && !breaks.isEmpty()) {
+            model.addAttribute("breakstatus", true);
+        }else{
+            model.addAttribute("breakstatus", false);
+        }
         // List<Break> breaks = breakService.findAll();
         // model.addAttribute("breaks", breaks);
         return "index"; // Return the Thymeleaf template name

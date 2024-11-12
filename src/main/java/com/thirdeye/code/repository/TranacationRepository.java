8eec1a827622591025accbd4dfb04bc95ca8f005
Zwe Lwin Oo <167851320+zwe-lwin-00@users.noreply.github.com>
2024-11-12 09:08:08 +0630

    Update TranacationRepository.java

--- src/main/java/com/thirdeye/code/repository/TranacationRepository.java
+++ src/main/java/com/thirdeye/code/repository/TranacationRepository.java
@@ -7,21 +7,17 @@ import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.query.Param;
 import org.springframework.stereotype.Repository;
 
-import com.thirdeye.code.dto.TranacationSummary;
-import com.thirdeye.code.entity.Tranacation;
+import com.thirdeye.code.entity.Transaction;
 
 @Repository
-public interface TranacationRepository extends JpaRepository<Tranacation, Long> {
+public interface TransactionRepository extends JpaRepository<Transaction, Long> {
 
-   // @Query("SELECT t FROM Tranacation t WHERE t.buynumber = :luckyNumber")
-   // List<Tranacation> findtransbynumber(@Param("luckyNumber") int luckyNumber);
+    @Query("SELECT t FROM Transaction t WHERE t.buynumber = :luckyNumber")
+    List<Transaction> findalltransactions(@Param("luckyNumber") int luckyNumber);
 
-   @Query("SELECT t FROM Tranacation t WHERE t.buynumber = :luckyNumber")
-   List<Tranacation> findalltranscations(@Param("luckyNumber") int luckyNumber);
-
-   @Query("SELECT t.buynumber AS buynumber, t.customer AS customer, SUM(t.amount) AS totalAmount " +
-         "FROM Tranacation t WHERE t.buynumber = :luckyNumber " +
-         "GROUP BY t.buynumber, t.customer")
-   List<Object[]> findtransgroupbycustomer(@Param("luckyNumber") int luckyNumber);
+    @Query("SELECT t.buynumber AS buynumber, t.customer AS customer, SUM(t.amount) AS totalAmount "
+            + "FROM Transaction t WHERE t.buynumber = :luckyNumber "
+            + "GROUP BY t.buynumber, t.customer")
+    List<Object[]> findtransgroupbycustomer(@Param("luckyNumber") int luckyNumber);
 
 }
